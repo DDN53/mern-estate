b2b32cc55384d3f227c05daf1c07e2f007555bd2
DDN53 <dinukanisheda809@gmail.com>
2024-04-03 09:29:19 +0530

    complete profile page Ui

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,5 +1,42 @@
-import React from "react";
+import { useSelector } from "react-redux";
 
 export default function Profile() {
-  return <div>Profile</div>;
+  const { currentUser } = useSelector((state) => state.user);
+  return (
+    <div className="max-w-lg p-3 mx-auto">
+      <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
+      <form className="flex flex-col gap-4">
+        <img
+          src={currentUser.avatar}
+          alt="profile"
+          className="self-center object-cover w-32 h-32 mt-2 rounded-full cursor-pointer bg-slate-400"
+        />
+        <input
+          type="text"
+          placeholder="username"
+          id="username"
+          className="p-3 border rounded-lg"
+        />
+        <input
+          type="text"
+          placeholder="email"
+          id="email"
+          className="p-3 border rounded-lg"
+        />{" "}
+        <input
+          type="text"
+          placeholder="password"
+          id="password"
+          className="p-3 border rounded-lg"
+        />
+        <button className="p-3 text-white uppercase rounded-lg bg-slate-700 hover:opacity-95 disabled:opacity-85">
+          update
+        </button>
+      </form>
+      <div className="flex justify-between mt-5">
+        <span className="text-red-700 cursor-pointer">Delete account</span>
+        <span className="text-red-700 cursor-pointer">Sign out</span>
+      </div>
+    </div>
+  );
 }
